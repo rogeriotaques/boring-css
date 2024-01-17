b51d71cef70ccf2c36e231b5866ecce545265588
Rogerio Taques <1456123+rogeriotaques@users.noreply.github.com>
2024-01-18 02:18:15 +0900

    fix: boring improvements

--- boring.js
+++ boring.js
@@ -10,6 +10,8 @@
 'use strict';
 
 (() => {
+  /* Dark Mode Toggle */
+
   const DM_STORAGE_KEY = 'darkModeOn';
   const THEME = Object.freeze({ DARK: 'dark', LIGHT: 'light' });
 
@@ -61,4 +63,41 @@
   });
 
   modeChangeHandler();
+
+  /* Tool Tip Auto Positioning */
+
+  const tooltips = document.querySelectorAll('.has-tooltip');
+
+  const repositionTooltips = () => {
+    tooltips.forEach((tooltip) => {
+      const curPos = tooltip.getBoundingClientRect();
+      const isOnTop = tooltip.classList.contains('has-tooltip--top');
+      const isOnRight = tooltip.classList.contains('has-tooltip--right');
+      const isAtBottom = tooltip.classList.contains('has-tooltip--bottom');
+      const isOnLeft = tooltip.classList.contains('has-tooltip--left');
+
+      if (isOnLeft && curPos.left <= 240) {
+        tooltip.classList.add('has-tooltip--right');
+        tooltip.classList.remove('has-tooltip--left');
+      }
+
+      if (isOnRight && curPos.right + 240 >= window.innerWidth) {
+        tooltip.classList.add('has-tooltip--left');
+        tooltip.classList.remove('has-tooltip--right');
+      }
+
+      if (isOnTop && curPos.top <= 50) {
+        tooltip.classList.add('has-tooltip--bottom');
+        tooltip.classList.remove('has-tooltip--top');
+      }
+
+      if (isAtBottom && curPos.bottom + 50 >= window.innerHeight) {
+        tooltip.classList.add('has-tooltip--top');
+        tooltip.classList.remove('has-tooltip--bottom');
+      }
+    });
+  };
+
+  window.addEventListener('resize', repositionTooltips);
+  window.addEventListener('scroll', repositionTooltips);
 })();
