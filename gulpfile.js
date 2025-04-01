592158d51d33ffb487fb601f5bf7b5349a634943
Rogerio Taques <1456123+rogeriotaques@users.noreply.github.com>
2025-04-01 22:27:05 +0900

    Tweaks to preserve some content in the outputted boring.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const header = require("gulp-header");
 const sourcemaps = require("gulp-sourcemaps");
 const browserSync = require("browser-sync").create();
 
@@ -7,6 +8,17 @@ const paths = {
   scss: ["src/*.scss", "!src/boring/**/*.scss"], // Path to your SCSS files
   css: "./", // Output directory for CSS
 };
+
+const headerString = [
+  "/**",
+  "* The Boring CSS",
+  "* A CSS boilerplate for NeoBrutalism styled pages.",
+  "*",
+  "* @author: Rogerio Taques",
+  "* @license: MIT",
+  "*/\n",
+  '@charset "UTF-8";\n',
+].join("\n");
 
 function compileSass() {
   return gulp
@@ -18,6 +30,7 @@ function compileSass() {
         outputStyle: "expanded", // This helps ensure charset is preserved
       }).on("error", sass.logError)
     )
+    .pipe(header(headerString))
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest(paths.css));
 }
